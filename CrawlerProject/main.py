ad398fc41799f83acf376b430d175a74850eacf3
Aleksander Jenssen <aleksander-j29@hotmail.com>
2026-03-29 15:13:04 +0200

    Cleaner logging
    Added the rich library for cleaner logging.

--- CrawlerProject/main.py
+++ CrawlerProject/main.py
@@ -3,8 +3,16 @@ import requests
 from queue import Queue
 from urllib.parse import urljoin
 import logging
+from rich.console import Console
+from rich.table import Table
 logger = logging.getLogger(__name__)
 
+console = Console();
+table = Table(title="Crawler Status")
+
+found_recipes = Table(title="Scraped Recipes")
+found_recipes.add_column("Scraped", style="cyan")
+found_recipes.add_column("Status", style="green")
 
 def main():
     logging.basicConfig(filename="Crawler.log", level=logging.INFO)
@@ -13,9 +21,16 @@ def main():
     crawl_queue.put(start_url)
     logger.info(f"Initializing crawler with starting url: {start_url}")
     crawling = True
-    logger.info("Crawler starting..")
+    table.add_column("Task", style="cyan")
+    table.add_column("Status", style="green")
+    table.add_row("Crawler Initialized", "✅ Done")
+    table.add_row("Crawler", "⏳ Running")
+    console.print(table)
+
+
 
     while crawling:
+        extracted_recipes = 0
         next_url = crawl_queue.get()
         try: 
             response = requests.get(next_url)
@@ -42,17 +57,23 @@ def main():
 
         try:
             recipe_name = soup.find('h1', class_='RecipeHeader_recipe-header__title__12vm2').get_text()
+            logger.info(f"Extracted recipe: {recipe_name}")
+            print("Recipe found: ", recipe_name)
+            found_recipes.add_row(f"{recipe_name}", "✅ Extracted")
         except AttributeError:
             recipe_name = "Unknown Recipe"
+            logger.error("Invalid url, moving on..")
         
-        print("Recipe Name: ", recipe_name)
-        logger.info(f"Extracted recipe: {recipe_name}")
         for ingredients in soup.find_all('ul', class_='RecipeIngredients_ingredients__list__02Pml'):
-            print(ingredients.get_text())
             open("recipes.txt", "a").write(f"Recipe Name: {recipe_name}\n")
             open("recipes.txt", "a").write(f"Ingredients: {ingredients.get_text()}\n\n")
             logger.info(f"Extracted ingredients for {recipe_name}")
     
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Crawler stopped!")
+        console.print(found_recipes)
+        print("Extracted recipes saved to recipes.txt")
